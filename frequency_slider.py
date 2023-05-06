b815424f6048bcd8d481256610672f7eb6eee4e1
Daniel <truran81@gmail.com>
2023-05-06 08:18:07 -0500

    Improved slider scale

--- frequency_slider.py
+++ frequency_slider.py
@@ -27,12 +27,28 @@ class App:
         self.slider_container = ttk.Frame(self.root)
         self.slider_container.pack(side=tk.TOP)
 
+    def slider_value_to_freq(self, value):
+        if value < 500:
+            return value
+        elif value < 1000:
+            return 500 + (value - 500) * 2
+        else:
+            return 1500 + (value - 1000) * 18.5
+
+    def freq_to_slider_value(self, freq):
+        if freq < 500:
+            return freq
+        elif freq < 1500:
+            return 500 + (freq - 500) / 2
+        else:
+            return 1000 + (freq - 1500) / 18.5
+
     def add_slider(self):
         slider_frame = ttk.Frame(self.slider_container)
         slider_frame.pack(side=tk.LEFT, padx=10)
 
-        max_freq = 20000
-        slider = tk.Scale(slider_frame, from_=max_freq, to=0, orient=tk.VERTICAL, length=200, command=self.update_frequency)
+        max_value = 20000
+        slider = tk.Scale(slider_frame, from_=self.freq_to_slider_value(max_value), to=self.freq_to_slider_value(1), orient=tk.VERTICAL, length=400, command=self.update_frequency, resolution=0.01, tickinterval=0, sliderlength=30, showvalue=0)
         slider.pack(side=tk.TOP)
 
         frequency_entry = tk.Entry(slider_frame, width=8)
@@ -45,14 +61,14 @@ class App:
 
     def update_frequency(self, value):
         for i, slider in enumerate(self.sliders):
-            freq = int(slider.get())
+            freq = int(self.slider_value_to_freq(float(slider.get())))
             self.frequency_entries[i].delete(0, tk.END)
             self.frequency_entries[i].insert(tk.END, str(freq))
 
     def set_frequency(self, event):
         for i, entry in enumerate(self.frequency_entries):
             freq = int(entry.get())
-            self.sliders[i].set(freq)
+            self.sliders[i].set(self.freq_to_slider_value(freq))
 
     def play_audio(self):
         if self.is_playing:
@@ -71,24 +87,4 @@ class App:
 
     def audio_callback(self, in_data, frame_count, time_info, status):
         frequencies = [float(entry.get()) for entry in self.frequency_entries]
-        t = np.linspace(0, 1, 44100, False)
-        signal = np.zeros_like(t)
-
-        for freq in frequencies:
-            if freq > 0:
-                signal += np.sin(freq * 2 * np.pi * t)
-
-        signal /= len(frequencies)
-        signal = signal.astype(np.float32)
-        return (signal.tobytes(), pyaudio.paContinue)
-
-    def on_closing(self):
-        self.stop_audio()
-        self.audio.terminate()
-        self.root.destroy()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.protocol("WM_DELETE_WINDOW", app.on_closing)
-    root.mainloop()
+        t
